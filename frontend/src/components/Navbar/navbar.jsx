9e54f5a1c00b05c5107be9eca21bcf42cfca8ea7
anuragbose124 <anuragbasu039@gmail.com>
2025-01-29 17:39:57 +0530

    work on registration form and navbar styeling and logo

--- frontend/src/components/Navbar/navbar.jsx
+++ frontend/src/components/Navbar/navbar.jsx
@@ -37,9 +37,12 @@ const Navbar = () => {
 
     return (
         <nav className="flex items-center justify-between p-4 shadow-lg bg-white rounded-full mx-4 lg:mx-auto lg:max-w-5xl relative z-50">
-            <div className="text-3xl font-bold text-teal-600 ml-4">
-                Adventor.
-            </div>
+            <Link to="/home">
+                <div className="text-3xl font-bold text-teal-600 ml-4">
+                    Adventor.
+                </div>
+            </Link>
+
 
             <div className="lg:hidden flex items-center">
                 <button onClick={toggleMenu} className="text-teal-600 text-3xl">
